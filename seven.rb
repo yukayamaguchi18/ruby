2ad8f0a71eb94947f229e15606314e8fd53a1522
yukayamaguchi18 <141097661+yukayamaguchi18@users.noreply.github.com>
2023-11-16 03:26:02 +0000

    [Add]surround

--- seven.rb
+++ seven.rb
@@ -2,8 +2,8 @@ min = 7
 max = 7777777
 count = 0
 
-(min..max).step(min).each do |num|
-  count += num.to_s.count('7')
+(min..max).step(min).each do |num| # 7～7777777まで7を足し続ける=7の倍数
+  count += num.to_s.count('7') # numの数字にいくつ7が含まれるか計算　count変数に追加する
 end
 
 puts "数字「7」は、#{count}回現れます。"
---
+++ surround.rb
@@ -0,0 +1,4 @@
+input_line = gets
+puts "+" * (input_line.length + 1)
+puts "+#{input_line.chomp}+"
+puts "+" * (input_line.length + 1)
