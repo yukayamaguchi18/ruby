ea8e3b507122a6cde63c5ac6f871d260a60f068c
yukayamaguchi18 <141097661+yukayamaguchi18@users.noreply.github.com>
2023-11-18 05:17:58 +0000

    [Fix]candy

--- candy.rb
+++ candy.rb
@@ -7,17 +7,17 @@ line1 = input_line[0].split(" ").map(&:to_i)
 $candy = []
 
 def candy(num)
-    if num == 1
-        $candy.insert(0, @first)
-    elsif num == @amount
-        $candy[num - 1] ||= @line2[num - 1].to_i - $candy[num - 2].to_i
+    if num == 0
+        $candy[0] ||= @first
+    elsif num == 1
+        $candy[1] ||= @line2[0].to_i - @first
     else
-        $candy[num - 1] ||= @line2[num - 1].to_i - $candy[num - 2].to_i - $candy[num].to_i
+        $candy[num] ||= @line2[num - 1].to_i - $candy[num - 2].to_i - $candy[num - 1].to_i
     end
 end
 
-i = 1
-while i <= @amount
+i = 0
+while i <= @amount - 1
     candy(i)
     i += 1
 end
